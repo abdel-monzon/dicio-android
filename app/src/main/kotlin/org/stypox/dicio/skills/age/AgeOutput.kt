1fab14b424273a075e797612df111ce83504e69d
abdel-monzon <146785152+abdel-monzon@users.noreply.github.com>
2025-11-23 00:52:05 -0500

    Update AgeOutput.kt

--- app/src/main/kotlin/org/stypox/dicio/skills/age/AgeOutput.kt
+++ app/src/main/kotlin/org/stypox/dicio/skills/age/AgeOutput.kt
@@ -1,25 +1,21 @@
 package org.stypox.dicio.skills.age
 
-import org.stypox.dicio.skills.SkillOutput
-import android.content.Context
+import org.dicio.skill.context.SkillContext
+import org.stypox.dicio.io.graphical.HeadlineSpeechSkillOutput
 
 class AgeOutput(
     private val years: Int,
-    private val months: Int, 
+    private val months: Int,
     private val days: Int
-) : SkillOutput {
+) : HeadlineSpeechSkillOutput {
     
-    override fun speak(context: Context): String {
+    override fun getSpeechOutput(ctx: SkillContext): String {
         return if (years > 0) {
             "Has vivido $years años, $months meses y $days días"
         } else if (months > 0) {
-            "Has vivido $months meses y $days días"  
+            "Has vivido $months meses y $days días"
         } else {
             "Has vivido $days días"
         }
-    }
-    
-    override fun display(context: Context): String {
-        return speak(context)
     }
 }
