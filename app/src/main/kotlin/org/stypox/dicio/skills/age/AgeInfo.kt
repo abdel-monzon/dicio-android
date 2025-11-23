744921a01c839ca78ff0bc19fbd36048092f79be
abdel-monzon <146785152+abdel-monzon@users.noreply.github.com>
2025-11-22 23:27:09 -0500

    Update AgeInfo.kt

--- app/src/main/kotlin/org/stypox/dicio/skills/age/AgeInfo.kt
+++ app/src/main/kotlin/org/stypox/dicio/skills/age/AgeInfo.kt
@@ -5,24 +5,22 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Cake
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
-import org.dicio.skill.context.SkillContext
-import org.dicio.skill.skill.Skill
-import org.dicio.skill.skill.SkillInfo
+import org.stypox.dicio.R
+import org.stypox.dicio.skills.SkillInfo
+import org.stypox.dicio.skills.Skill
 import org.stypox.dicio.sentences.Sentences
 
-object AgeInfo : SkillInfo("age") {
-    override fun name(context: Context) = "Calculadora de edad"
+object AgeInfo : SkillInfo() {
+    override fun name(context: Context): String = context.getString(R.string.skill_age_name)
     
-    override fun exampleSentence(context: Context) = "cuántos días tengo"
+    override fun sentenceExample(context: Context): String = context.getString(R.string.skill_age_example)
     
     @Composable
     override fun icon() = rememberVectorPainter(Icons.Default.Cake)
     
-    override fun isAvailable(ctx: SkillContext): Boolean {
+    override fun isAvailable(ctx: org.stypox.dicio.skills.SkillContext): Boolean {
         return Sentences.Age[ctx.sentencesLanguage] != null
     }
     
-    override fun build(ctx: SkillContext): Skill<*> {
-        return AgeSkill(AgeInfo, Sentences.Age[ctx.sentencesLanguage]!!)
-    }
+    override fun skill(): Skill<*> = AgeSkill()
 }
